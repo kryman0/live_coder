1d755ef34ca03fa0fd0e12d677697d98561d50fc
Krystian Manczak <kryman0@gmail.com>
2021-12-25 19:57:17 +0100

    Added util.promisify for awaiting the asynchronous result when getting the stdout from a child process.

--- app.mjs
+++ app.mjs
@@ -1,6 +1,8 @@
 import express from "express";
 import fs from "fs";
 import childProc from "child_process";
+import util from "util";
+
 
 const app = express();
 const port = 5000;
@@ -11,7 +13,7 @@ app.use(express.text(
 ));
 
 
-app.post("/", (req, res) => {
+app.post("/", async (req, res) => {
     //console.log(req.body);
 
     fs.writeFile("./test.php", req.body, (err) => {
@@ -20,17 +22,51 @@ app.post("/", (req, res) => {
         }
     });
 
-    const child = childProc.execFile("php", [ "-f", "./test.php" ], (err, stdout, stderr) => {
-        if (err || stderr) console.log("error exec file", err, stderr);
-       
-        //res.send(stdout);
-    });
+//    const child = childProc.execFile("php", [ "-f", "./test.php" ], (err, stdout, stderr) => {
+//        if (err || stderr) console.log("error exec file", err, stderr);
+//       
+//        //res.send(stdout);
+//    });
+    
+    
+    const child = await getOutput("./test.php");
 
-    res.send(child.stdout.on("data", (chunk) => { return chunk; }));
+    //console.log(child.on("data", (data) => data));
+    console.log(child);
+
+    res.send("it works");
 });
 
 
-//function saveFile()
+async function getOutput(file) {
+//    const execFile = new Promise((res, rej) => {
+//        return childProc.execFile;
+//    });
+//
+//    const promise = await new Promise((res, rej) => {
+//        res(childProc.execFile("php", [ "-f", file ]));
+//    });
+//
+    const execFile = util.promisify(childProc.execFile);
+
+    const { stdout } = await execFile("php", [ "-f", file ]);
+//    const data = await stdout.on("data", (data) => {
+//        //console.log(data);
+//
+//        return data;
+//    });
+
+    //console.log(data);
+
+    //const { stdout } = await execFile("php", [ "-f", file ]);
+
+    //console.log(stdout);
+
+    //console.log(child);
+
+    return stdout;
+}
+
 
 app.listen(port, () => {
     console.log(`App listens on http://localhost:${port}`);
