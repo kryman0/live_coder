06ed9692f481242df784dd4d633b614f804b1b85
Krystian Manczak <kryman0@gmail.com>
2021-12-25 13:54:13 +0100

    Added saving file and reading of stdout in console.

--- app.mjs
+++ app.mjs
@@ -1,26 +1,36 @@
 import express from "express";
-
 import fs from "fs";
+import childProc from "child_process";
 
 const app = express();
-
 const port = 5000;
 
 
-app.get("/", (req, res) => {
-    fs.writeFile("./test.php", req.query, (err) => {
-        if (err) {
-            console.log("Something went wrong");
-        }
+app.use(express.text(
+    { limit: "500kb" }
+));
 
-        // execute file with "php -f <file>"
+
+app.post("/", (req, res) => {
+    //console.log(req.body);
+
+    fs.writeFile("./test.php", req.body, (err) => {
+        if (err) {
+            res.send("Something went wrong");
+        }
     });
 
-    res.send("it works");
+    const child = childProc.execFile("php", [ "-f", "./test.php" ], (err, stdout, stderr) => {
+        if (err || stderr) console.log("error exec file", err, stderr);
+       
+        //res.send(stdout);
+    });
+
+    res.send(child.stdout.on("data", (chunk) => { return chunk; }));
 });
 
 
-function saveFile()
+//function saveFile()
 
 app.listen(port, () => {
     console.log(`App listens on http://localhost:${port}`);
