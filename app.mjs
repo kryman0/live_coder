b7077a12fee42cd4212293905afa9270074fa2f1
Krystian Manczak <kryman0@gmail.com>
2021-12-31 19:10:30 +0100

    Added functions for saving and fetching files. Added language array for available languages.

--- app.mjs
+++ app.mjs
@@ -7,6 +7,11 @@ import util from "util";
 const app = express();
 const port = 5000;
 
+app.use(
+    express.json(
+        { limit: "500kb" }
+    )
+);
 
 app.use(
     express.static(
@@ -14,11 +19,11 @@ app.use(
     )
 );
 
-app.use(
-    express.text(
-        { limit: "500kb" }
-    )
-);
+//app.use(
+//    express.text(
+//        { limit: "500kb" }
+//    )
+//);
 
 app.set("views", "./views"); // views directory
 app.set("view engine", "pug"); // template engine
@@ -30,18 +35,52 @@ app.get("/", (req, res) => {
 
 app.post("/", async (req, res) => {
     //console.log(req.body);
+    let stdMsg = "";
+    
+    // Check file was written and language exists."
+    if (await getWrittenFile(req.body) && getRightFile(req.body.mode)) {
+        let file = createFileName(req.body.mode);
+        
+        stdMsg = await getOutput(`./temp/${file}`);
+        //console.log(file, stdMsg);
+    } else {
+        stdMsg = "Error, please try again!";
+    }
 
-    fs.writeFile("./temp/test.php", req.body, (err) => {
-        if (err) {
-            throw err;
-        }
-    });
-
-    const stdMsg = await getOutput("./temp/test.php");
-
-
+    
     res.send(stdMsg);
 });
+
+
+function getRightFile(fileExt) {
+    const availableLanguages = [ "php" ];
+
+
+    return availableLanguages.find((el) => el == fileExt);
+}
+
+
+async function getWrittenFile(body) {
+    const filesFolder = "./temp/";
+    
+    var isFileSaved = false;
+
+    let file = createFileName(body.mode);
+    //console.log(filesFolder + file);
+    
+    try {
+        isFileSaved = await fs.promises.writeFile(filesFolder + file, body.code);
+        //console.log("try:", isFileSaved);
+    } catch (error) {
+        //console.log("catch", error);
+        isFileSaved = false;
+    }
+
+    
+    //console.log("function", isFileSaved);
+
+    return isFileSaved === undefined ? true : false;
+}
 
 
 async function getOutput(file) {
@@ -50,7 +89,7 @@ async function getOutput(file) {
     let [ child, stderr ] = [ "", "" ];
 
     try {
-        child = await execFile("php", [ "-f", file ]);
+        child = await execFile("php", [ "-f", file ]); // ändra efter filändelse för rätt program
     } catch(error) {
         stderr = error.stderr;
         //console.log(error);
@@ -59,6 +98,11 @@ async function getOutput(file) {
     if (stderr) return stderr;
 
     return child.stdout;
+}
+
+
+function createFileName(file) {
+    return "code." + file;
 }
 
 
--- public/js/edit_code.js
+++ public/js/edit_code.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var textareaElem = document.getElementById("edit-code");
 
 let cmOpts = {
@@ -15,19 +17,59 @@ var codeMirror = CodeMirror.fromTextArea(textareaElem, cmOpts);
 
 function sendCode(text) {
     let codeResElem = document.getElementById("code-result");
-    //console.log("from function:", text);
+
+    let bodyObj = {
+        code: text,
+        mode: codeMirror.getOption("mode")
+    };
+
+    //console.log(bodyObj);
+
+    if (bodyObj.code == "" || bodyObj.mode === null) {
+        codeResElem.className = "error";
+
+        codeResElem.innerText = "Please check that code is written " +
+            "and language has been chosen!";
+
+        return;
+    }
+    
 
     fetch("http://localhost:5000", {
         method: "POST",
-        headers: { "Content-Type": "text/plain" },
-        body: text
+        headers: { "Content-Type": "application/json" },
+        //headers: { "Content-Type": "text/plain" },
+        body: JSON.stringify(bodyObj)
+        //body: bodyObj
     }).then(
         resp => resp.text()
     ).then(data => {
+        if (data.startsWith("Error"))
+            codeResElem.className = "error";
+        else
+            codeResElem.style.border = "none";
+
         codeResElem.innerText = data;
     }).catch(err => console.log("Something went wrong:", err));
 }
 
-function setcmMode(mode) {
-    console.log(mode);
+
+function setcmMode(eventElem) {
+    let langMenuElem = document.getElementById("lang-menu");
+
+    let className = "";
+
+    if (eventElem.target.tagName !== "LI") {
+        className = "enable-display";
+    } else {
+        className = "remove-display";
+
+        codeMirror.setOption("mode", eventElem.target.innerText);
+    }
+
+    for (let i = 0; i < langMenuElem.children.length; i++) {
+        langMenuElem.children[i].className = className;
+    }
+
+    console.log(eventElem);
 }
